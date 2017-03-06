5a3e048920f08c26c466a6793afe83b005a14f0b
Jarett DeAngelis <jarett@reticulum.use>
2017-03-06 14:25:53 -0500

    Start of Gallery Controller

---
+++ controllers/galleries.go
@@ -0,0 +1,24 @@
+package controllers
+
+import "github.com/jdkruzr/web/views"
+import "net/http"
+import "fmt"
+
+// type SignupForm struct {
+//	Email	string `schema:"email"`
+//	Password string `schema:"password"`
+//}
+
+type Galleries struct {
+	NewView *views.View
+}
+
+func NewGalleries() *Galleries {
+	return &Galleries{
+		NewView: views.NewView("bootstrap", "galleries/new"),
+	}
+}
+
+func (g *Galleries) New(w http.ResponseWriter, r *http.Request) {
+	g.NewView.Render(w, nil)
+}
